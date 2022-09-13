2331f06effee12aad78be791a979f8a654db7803
Muhamad Azamy <muhamad@incubaid.com>
2022-09-13 17:16:11 +0200

    Working on pool implementation

---
+++ src/storage/pool/mod.rs
@@ -0,0 +1,49 @@
+use crate::Unit;
+/// a pool is a wrapper around a disk device. right now a single pool
+/// uses a single disk device.
+use std::path::Path;
+use thiserror::Error;
+// define error type?
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("volume not found {volume}")]
+    VolumeNotFound { volume: String },
+    #[error("pool not found {pool}")]
+    PoolNotFound { pool: String },
+    //todo: add more errors based on progress
+
+    // cover it all error
+    #[error("{0}")]
+    Other(#[from] anyhow::Error),
+}
+
+type Result<T> = anyhow::Result<T, Error>;
+
+#[async_trait::async_trait]
+pub trait Volume {
+    fn id(&self) -> u64;
+
+    fn path(&self) -> &Path;
+
+    fn name(&self) -> &str;
+
+    async fn limit(&self, size: Unit) -> Result<()>;
+
+    async fn usage(&self) -> Result<Unit>;
+}
+
+#[async_trait::async_trait]
+pub trait Pool: Volume {
+    type Volume: Volume;
+
+    async fn mount(&self) -> Result<&Path>;
+
+    async fn unmount(&self) -> Result<()>;
+
+    async fn volumes(&self) -> Result<Vec<Self::Volume>>;
+
+    async fn volume<S: AsRef<str>>(&self, name: S) -> Result<Self::Volume>;
+
+    async fn delete<S: AsRef<str>>(&self, name: S) -> Result<()>;
+}
