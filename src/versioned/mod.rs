1362da917502bba7813dfce65128c9745dfa03ba
mariobassem <mariobassem12@gmail.com>
2022-09-28 10:59:18 +0200

    remove VersionedReader

--- src/versioned/mod.rs
+++ src/versioned/mod.rs
@@ -1,7 +1,6 @@
 use anyhow::Context;
 use semver::Version;
 use std::fs::Permissions;
-use std::ops::{Deref, DerefMut};
 use std::path::Path;
 use std::str::{self, FromStr};
 use std::{fmt::Debug, os::unix::prelude::PermissionsExt};
@@ -34,94 +33,53 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// VersionedReader is a reader that can load the version of the data from a stream
-/// without assuming anything regarding the underlying encoding of your data object.
-pub struct VersionedReader<R>
-where
-    R: AsyncRead + Unpin,
-{
-    version: Version,
-    inner: R,
-}
-
-impl<R> Deref for VersionedReader<R>
-where
-    R: AsyncRead + Unpin,
-{
-    type Target = R;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
-    }
-}
-
-impl<R> DerefMut for VersionedReader<R>
-where
-    R: AsyncRead + Unpin,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.inner
-    }
-}
-
-impl<R> VersionedReader<R>
-where
-    R: AsyncRead + Unpin,
-{
-    /// Return the version of the data.
-    pub fn version(&self) -> &Version {
-        &self.version
-    }
-
-    /// Creates a new versioned reader from a stream. It fails
-    /// if the reader can not read the version from the stream.
-    /// On success, the reader will have a version, and then can be used
-    /// to load the data.
-    ///
-    /// The reader should be a type implementing [`AsyncRead`] + [`Unpin`].
-    ///
-    /// If parsing succeeds, returns `VersionedReader<R>` inside [`Ok`].
-    ///
-    /// # Errors
-    /// Returns `Err` if version information is not found or valid, or when there is an io error.
-    ///
-    /// # Example
-    /// ```ignore
-    /// let mut file = tokio::fs::OpenOptions::new()
-    ///     .read(true)
-    ///     .open(path.as_ref())
-    ///     .await?;
-    /// let mut reader = VersionedReader::new(&mut file).await?;
-    /// ```
-
-    pub async fn new(mut r: R) -> Result<VersionedReader<R>> {
-        let mut double_quotes: u8 = 0;
-        let mut version_bytes = Vec::<u8>::new();
-        for _ in 0..MAX_VERSION_LENGTH {
-            let byte = r.read_u8().await?;
-            if double_quotes == 0 && byte != b'\"' {
-                return Err(Error::NotVersioned);
-            }
-            if byte == b'\"' {
-                double_quotes += 1;
-                if double_quotes == 2 {
-                    break;
-                }
-                continue;
-            }
-            version_bytes.push(byte);
+/// Creates a new versioned reader from a stream. It fails
+/// if the reader can not read the version from the stream.
+/// On success, the reader will have a version, and then can be used
+/// to load the data.
+///
+/// The reader should be a type implementing [`AsyncRead`] + [`Unpin`].
+///
+/// If parsing succeeds, returns `VersionedReader<R>` inside [`Ok`].
+///
+/// # Errors
+/// Returns `Err` if version information is not found or valid, or when there is an io error.
+///
+/// # Example
+/// ```ignore
+/// let mut file = tokio::fs::OpenOptions::new()
+///     .read(true)
+///     .open(path.as_ref())
+///     .await?;
+/// let mut reader = VersionedReader::new(&mut file).await?;
+/// ```
+pub async fn reader<R: AsyncRead + Unpin>(mut r: R) -> Result<(Version, R)> {
+    let mut double_quotes: u8 = 0;
+    let mut version_bytes = Vec::<u8>::new();
+    for _ in 0..MAX_VERSION_LENGTH {
+        let byte = r.read_u8().await?;
+        if double_quotes == 0 && byte != b'\"' {
+            return Err(Error::NotVersioned);
         }
-        if double_quotes != 2 {
-            return Err(Error::VersionLengthExceeded);
+        if byte == b'\"' {
+            double_quotes += 1;
+            if double_quotes == 2 {
+                break;
+            }
+            continue;
         }
-        let version_str = str::from_utf8(&version_bytes)
-            .context("failed to convert version information to string")?;
-        let version = Version::from_str(version_str).map_err(|_| Error::InvalidVersion {
-            version: version_str.into(),
-        })?;
-
-        Ok(VersionedReader::<R> { version, inner: r })
+        version_bytes.push(byte);
     }
+    if double_quotes != 2 {
+        return Err(Error::VersionLengthExceeded);
+    }
+    let version_str = str::from_utf8(&version_bytes)
+        .context("failed to convert version information to string")?;
+    let version = Version::from_str(version_str).map_err(|_| Error::InvalidVersion {
+        version: version_str.into(),
+    })?;
+
+    Ok((version, r))
 }
 
 /// Reads versioned file's contents.
@@ -132,14 +90,14 @@ pub async fn read_file<P: AsRef<Path>>(path: P) -> Result<(Version, Vec<u8>)> {
         .read(true)
         .open(path.as_ref())
         .await?;
-    let mut reader = VersionedReader::new(&mut file).await?;
+    let (version, reader): (Version, &mut tokio::fs::File) = reader(&mut file).await?;
     let mut buf = Vec::new();
     reader.read_to_end(&mut buf).await?;
-    Ok((reader.version, buf))
+    Ok((version, buf))
 }
 
 /// Writes version to a writer implementing [`AsyncWrite`].
-pub async fn new_writer<W: AsyncWrite + Unpin>(mut w: W, version: &Version) -> Result<W> {
+pub async fn writer<W: AsyncWrite + Unpin>(mut w: W, version: &Version) -> Result<W> {
     let v_str = serde_json::json!(version.to_string());
     w.write_all(v_str.to_string().as_bytes()).await?;
     Ok(w)
@@ -159,7 +117,7 @@ pub async fn write_file<P: AsRef<Path>>(
         .write(true)
         .open(path.as_ref())
         .await?;
-    let mut file = new_writer(file, version).await?;
+    let mut file = writer(file, version).await?;
     file.write_all(data).await?;
     Ok(())
 }
@@ -167,7 +125,7 @@ pub async fn write_file<P: AsRef<Path>>(
 #[cfg(test)]
 
 mod test {
-    use super::{write_file, Error, VersionedReader};
+    use super::{read_file, write_file, Error};
     use rand::{self, Rng};
     use semver::Version;
     use std::io::Write;
@@ -178,7 +136,7 @@ mod test {
     async fn test_max_version_length() {
         let long_version: Vec<u8> = vec![b'a'; 100];
         let reader = format!("\"{}\"data", std::str::from_utf8(&long_version).unwrap());
-        let versioned = VersionedReader::new(reader.as_bytes()).await;
+        let versioned = super::reader(reader.as_bytes()).await;
         assert!(matches!(versioned, Err(Error::VersionLengthExceeded)))
     }
 
@@ -195,14 +153,14 @@ mod test {
 
     #[tokio::test]
     async fn test_invalid_version() {
-        let versioned = VersionedReader::new(r#""mario"abcdef"#.as_bytes()).await;
+        let versioned = super::reader(r#""mario"abcdef"#.as_bytes()).await;
 
         assert!(matches!(versioned, Err(Error::InvalidVersion{version}) if version == "mario"));
     }
 
     #[tokio::test]
     async fn test_unversioned() {
-        let versioned = VersionedReader::new(r#"1.2.3"abcdef"#.as_bytes()).await;
+        let versioned = super::reader(r#"1.2.3"abcdef"#.as_bytes()).await;
 
         assert!(matches!(versioned, Err(Error::NotVersioned)));
     }
@@ -212,7 +170,7 @@ mod test {
         let mut file = tempfile::NamedTempFile::new().unwrap();
         write!(file, r#""1.5.7-alpha"hello world"#).unwrap();
 
-        let (read_version, read_data) = super::read_file(file.path()).await.unwrap();
+        let (read_version, read_data) = read_file(file.path()).await.unwrap();
 
         let version = Version::from_str("1.5.7-alpha").unwrap();
         assert_eq!(version, read_version);
@@ -229,7 +187,7 @@ mod test {
         let res = write_file(file.path(), &version, &data, Permissions::from_mode(0400)).await;
         assert!(res.is_ok());
 
-        let (read_version, read_data) = super::read_file(file.path()).await.unwrap();
+        let (read_version, read_data) = read_file(file.path()).await.unwrap();
         assert_eq!(version, read_version);
         assert_eq!(data, read_data);
     }
